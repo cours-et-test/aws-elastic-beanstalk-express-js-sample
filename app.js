830eea90e206fce96d9908e6e9a107eb86325789
Habibou DIEDHIOU <habibodh@gmail.com>
2022-04-26 18:29:31 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Habibi'));
+app.get('/', (req, res) => res.send('Hello Habibo DIEDHIOU'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
